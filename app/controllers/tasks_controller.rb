2192cc67de3d2864650857cd632c0d2060e892fa
Justin <justinhwu95@gmail.com>
2019-06-19 12:35:58 -0400

    Finished building out CRU, missing the delete for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,17 @@ class TasksController < ApplicationController
     render json: @task
   end
 
+  def edit
+    render json: Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    render json: @task
+
+  end
+
   private
   def task_params
     params.require(:task).permit(:description, :due, :priority, :list_id)
